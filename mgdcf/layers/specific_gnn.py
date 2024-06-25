fdf22405c99abed9e7141c9ee784b255abe7b2c9
sunshb5 <118787167+sunshb5@users.noreply.github.com>
2024-06-26 04:39:11 +0800

    Update specific_gnn.py

--- mgdcf/layers/specific_gnn.py
+++ mgdcf/layers/specific_gnn.py
@@ -2,13 +2,16 @@ import tensorflow as tf
 from grecx.layers import LightGCN
 import tf_sparse as tfs
 
-
 class HeteroGNN(tf.keras.Model):
-    """
-    Each NGCF Convolutional Layer
-    """
 
     def __init__(self, dense_activation=tf.nn.leaky_relu, edge_drop_rate=0.0, *args, **kwargs):
+        """
+        初始化 HeteroGNN 模型。
+
+        参数:
+        - dense_activation (callable): 密集层激活函数。
+        - edge_drop_rate (float): 边的 dropout 率。
+        """
         super().__init__(*args, **kwargs)
 
         self.dense_activation = dense_activation
@@ -17,10 +20,28 @@ class HeteroGNN(tf.keras.Model):
         self.edge_drop_rate = edge_drop_rate
 
     def build(self, input_shape):
+        """
+        构建模型的层。
+
+        参数:
+        - input_shape (tuple): 输入数据的形状。
+        """
         x_shape, _ = input_shape
         self.gcn_dense = tf.keras.layers.Dense(x_shape[1], activation=self.dense_activation)
 
     def call(self, inputs, training=None, mask=None, cache=None):
+        """
+        执行模型的前向传播。
+
+        参数:
+        - inputs (list): 包含输入数据的列表。期望输入包含 (x, edge_index)。
+        - training (bool): 表示模型是否处于训练模式的标志。
+        - mask: 可选的掩码张量，用于屏蔽特定元素。
+        - cache: 用于存储中间结果的可选缓存。
+
+        返回:
+        - h: 经过模型操作后的处理输出张量。
+        """
         x, edge_index = inputs[0], inputs[1]
 
         num_nodes = tfs.shape(x)[0]
@@ -36,6 +57,15 @@ class HeteroGNN(tf.keras.Model):
 class HeteroGNNSpecific(tf.keras.Model):
 
     def __init__(self, k=4, z_drop_rate=0.0, edge_drop_rate=0.0, dense_activation=tf.nn.leaky_relu, *args, **kwargs):
+        """
+        初始化 HeteroGNNSpecific 模型。
+
+        参数:
+        - k (int): HeteroGNN 层的数量。
+        - z_drop_rate (float): 输出特征的 dropout 率。
+        - edge_drop_rate (float): 边的 dropout 率。
+        - dense_activation (callable): 密集层激活函数。
+        """
         super().__init__(*args, **kwargs)
 
         self.hetero_gcns = [HeteroGNN(dense_activation=dense_activation, edge_drop_rate=edge_drop_rate) for _ in range(k)]
@@ -45,6 +75,18 @@ class HeteroGNNSpecific(tf.keras.Model):
             setattr(self, "hetero_gcn{}".format(i), hetero_gcn)
 
     def call(self, inputs, training=None, mask=None, cache=None):
+        """
+        执行模型的前向传播。
+
+        参数:
+        - inputs (list): 包含输入数据的列表。期望输入包含 (x, edge_index)。
+        - training (bool): 表示模型是否处于训练模式的标志。
+        - mask: 可选的掩码张量，用于屏蔽特定元素。
+        - cache: 用于存储中间结果的可选缓存。
+
+        返回:
+        - h: 经过模型操作后的处理输出张量。
+        """
         x, edge_index = inputs
 
         h = x
